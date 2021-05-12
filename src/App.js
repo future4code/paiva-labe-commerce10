97629e0f1a26fdbfd9d21986a36d8afe29b1e84d
Pedro Rodrigues <pr.rodriguesb@outlook.com>
2021-05-12 15:26:47 -0300

    base app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
 import React from 'react';
-import Carrinho from './components/carrinho/carrinho'
-import './App.css';
+import { Filtros } from './components/Filtros/Filtros';
+import { Produtos } from './components/Produtos/Produtos';
+import { Carrinho } from './components/Carrinho/Carrinho';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    
-  );
+const ContainerAplicacao = styled.div`
+`
+
+const produtos = [
+  {
+    id: Date.now(),
+    nome: 'Produto Teste',
+    preco: 29.99,
+    foto: 'https://miro.medium.com/max/640/0*i1v1In2Tn4Stnwnl.jpg'
+  }
+
+]
+
+class App extends React.Component {
+  state = {
+    filtroMin: 0,
+    filtroMax: 0,
+    textoFiltro: "",
+    produtosCarrinho: [
+      {
+        id: Date.now(),
+        nome: 'Produto Teste',
+        preco: 29.99,
+        foto: 'https://miro.medium.com/max/640/0*i1v1In2Tn4Stnwnl.jpg',
+        quantidade: 1
+      }
+    ]
+  }
+
+  render() {
+    return (
+      <ContainerAplicacao>
+        <Filtros />
+        <Produtos />
+        <Carrinho />
+      </ContainerAplicacao>
+    )
+  }
 }
+
+export default App;
+
 
