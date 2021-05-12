e84c6a69a4215d9938d4b93ae4da9c1d4b551d2f
SilvioDiaz <57494579+SilvioDiaz@users.noreply.github.com>
2021-05-12 17:46:44 -0300

    Inicio de Function e State

--- src/components/Carrinho/Carrinho.js
+++ src/components/Carrinho/Carrinho.js
@@ -2,15 +2,34 @@ import React from "react";
 import {CarrinhoArea,CarrinhoButton,CarrinhoProduto,CarrinhoTexto} from './styled'
 
 export default class Carrinho extends React.Component{
+
+    state = {
+        compra:[
+            {idCompra: Date.now(), qntdCompra = 2, nomeProduto = "Tenis", valorProduto = 10}
+        ]
+    }
+
+    duplicarValor = () =>{
+
+    }
+
+    valorTotal = () => {
+
+    }
+
+    apagarProduto = () => {
+
+    }
+
     render(){
         return(
             <CarrinhoArea>
                 <CarrinhoProduto>
-                    <CarrinhoTexto>Quantidade Produto</CarrinhoTexto>
-                    <CarrinhoTexto>Nome Produto</CarrinhoTexto>
-                    <CarrinhoButton>Apagar Produto</CarrinhoButton>
+                    <CarrinhoTexto>{this.props.qntProduto}</CarrinhoTexto>
+                    <CarrinhoTexto>{this.props.nomeProduto}</CarrinhoTexto>
+                    <CarrinhoButton onClick = {this.props.btnApagar}>Apagar Produto</CarrinhoButton>
                 </CarrinhoProduto>
-                <CarrinhoTexto>Total total:</CarrinhoTexto>
+                <CarrinhoTexto>Total total: {this.valorTotal}</CarrinhoTexto>
             </CarrinhoArea>            
         )
     }
