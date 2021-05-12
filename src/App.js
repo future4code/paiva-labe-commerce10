d175cb2c9989042d74e4b938c87a20ac3447f35d
SilvioDiaz <57494579+SilvioDiaz@users.noreply.github.com>
2021-05-12 18:14:58 -0300

    Carrinho dinamico

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Carrinho from './components/Carrinho/Carrinho';
 import foto1 from './assets/image1.png'
 import foto2 from './assets/image2.png'
 import foto3 from './assets/image3.png'
-import foto4 from './assets/image4.png'
-import foto5 from './assets/image5.png'
-import foto6 from './assets/image6.png'
+// import foto4 from './assets/image4.png'
+// import foto5 from './assets/image5.png'
+// import foto6 from './assets/image6.png'
 import styled from 'styled-components'
 
 const ContainerAplicacao = styled.div`
@@ -44,16 +44,17 @@ export default class App extends React.Component {
     textoFiltro: "",
     produtosCarrinho: [
      {
-    id: 1,
-    nome: 'Tênis Nike Lebron Witness V',
-    preco: 351,
-    foto: { foto1 }
+    idProduto: 1,
+    nomeProduto: 'Tênis Nike Lebron Witness V',
+    valorProduto: 351,
+    qntdCompra: 2,
+
   }
     ]
   }
 
   addProdutoAoCarrinho = (idProduto) => {
-
+    console.log(idProduto)
   }
 
   render() {
@@ -63,7 +64,10 @@ export default class App extends React.Component {
         <Produtos 
       produtos={produtos}
       />
-        <Carrinho />
+        <Carrinho
+        carrinhoCompra = {this.state.produtosCarrinho}
+        />
+        
       </ContainerAplicacao>
     )
   }
