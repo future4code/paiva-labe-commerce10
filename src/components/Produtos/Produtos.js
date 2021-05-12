d3ee0c3047271c489aabf28f12a8cbdeb41dd499
Renato Marinho <renato.marinhofr@gmail.com>
2021-05-12 13:45:32 -0300

    Alterado componente carrinho para Maiusculo

--- src/components/Produtos/Produtos.js
+++ src/components/Produtos/Produtos.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ListaProdutos } from './ListaProdutos'
-import Styled from 'styled-components'
+import styled from 'styled-components'
 
 const ContainerProdutos = styled.div`
   
