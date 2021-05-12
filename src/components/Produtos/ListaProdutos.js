3327e5b1b877f46feed12813ebb590985ee72afb
Renato Marinho <renato.marinhofr@gmail.com>
2021-05-12 12:47:13 -0300

    Adicionado algumas estilizações nos produtos

--- src/components/Produtos/ListaProdutos.js
+++ src/components/Produtos/ListaProdutos.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import Styled from 'styled-components'
+import styled from 'styled-components'
 
 const Item = styled.div`
-  
+  border: 1px solid;
+  display: flex;
+  flex-direction: column;
+  img {
+    
+  }
 `
 const InfoProduto = styled.div`
-
+  display: flex;
+  padding: 1rem;
+  flex-direction: column;
+  p {
+    margin: 0.25rem 0;
+  }
 `
 
 const AdicionaProduto = styled.button`
+  border: none;
 
 `
 
@@ -21,6 +32,9 @@ export class ListaProdutos extends React.Component {
           <p>{NOME DO PRODUTO}</p>
           <p>{PREÇO DO PRODUTO}</p>
         </InfoProduto>
+        <AdicionaProduto onClick={() => this.props.addProdutoAoCarrinho(produto.id)}>
+          Comprar
+        </AdicionaProduto>
       </Item>
       
     )
--- src/components/Produtos/Produtos.js
+++ src/components/Produtos/Produtos.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { ListaProdutos } from './ListaProdutos'
-import Styled from 'styled-components'
+import styled from 'styled-components'
 
 const ContainerProdutos = styled.div`
-  
+
 `
 const HeaderProdutos = styled.div`
-
+  display: flex;
+  align-items: left;
+  padding: 0 1rem;
 `
 const OrganizaProdutos = styled.div`
-
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 1rem;
+  padding: 1rem;
 `
 
 
@@ -20,7 +25,7 @@ export default class Produtos extends React.Component {
         <HeaderProdutos>
           <p>Mostrando: {QUANTIDADE DE PRODUTOS} Produtos</p>
           <label>
-            <span>Ordenar: </span>
+            <span>Ordenar por: </span>
             <select>
               <option>Crescente</option>
               <option>Decrescente</option>
