844d337f7612464ba22410058466c55a545e4163
Pedro Rodrigues <pr.rodriguesb@outlook.com>
2021-05-14 23:54:31 -0300

    atualização renato estilos

--- src/components/Carrinho/styled.js
+++ src/components/Carrinho/styled.js
@@ -1,22 +1,82 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import { darken } from "polished";
 
-export const CarrinhoArea = styled.div`
-       background:red;
-` 
+export const Container = styled.div`
+  padding: 30px;
+  background: #fff;
+  border-radius: 4px;
+  footer {
+    margin-top: 30px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    button {
+      background: #191920;
+      color: #fff;
+      border: 0;
+      border-radius: 4px;
+      padding: 12px 20px;
+      font-weight: bold;
+      text-transform: uppercase;
+      transition: background 0.2s;
+      &:hover {
+        background: ${darken(0.06, "#191920")};
+      }
+    }
+  }
+`;
 
-export const CarrinhoProduto = styled.div`
-    background:blue;    
-`
+export const ProductTable = styled.table`
+  width: 100%;
+  thead th {
+    color: #999;
+    text-align: left;
+    padding: 12px;
+  }
+  tbody td {
+    padding: 12px;
+    border-bottom: 1px solid #eee;
+  }
+  img {
+    height: 100px;
+  }
+  strong {
+    color: #333;
+    display: block;
+  }
+  span {
+    display: block;
+    margin-top: 5px;
+    font-size: 18px;
+    font-weight: bold;
+  }
+  div {
+    display: flex;
+    align-items: center;
+    input {
+      border: 1px solid #ddd;
+      border-radius: 4px;
+      color: #666;
+      padding: 6px;
+      width: 50px;
+    }
+  }
+  button {
+    background: none;
+    border: 0;
+    padding: 6px;
+  }
+`;
 
-export const CarrinhoButton = styled.button`
-    background: pink;
-`
-
-
-export const CarrinhoTexto = styled.p`
-    color:white;
-`
-
-export const InputQntd = styled.input`
-    border: solid 1px black
-`
+export const Total = styled.div`
+  display: flex;
+  align-items: baseline;
+  span {
+    color: #999;
+    font-weight: bold;
+  }
+  strong {
+    font-size: 28px;
+    margin-left: 5px;
+  }
+`;
